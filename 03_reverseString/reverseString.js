8b0447eb3f51a44e8c353bd5878de18b4f65ce81
Similo <171865838+Chief-99@users.noreply.github.com>
2025-01-09 01:22:35 +0200

    03: Create loop to print string in reverse and return result

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,15 @@
 const reverseString = function(string) {
   const l = string.length;
   let result = '';
-  for (let i = 0; i < l; i++) {
 
+  for (let i = l - 1; i >= 0; i--) {
+    result += string[i];
   }
+
+  return result;
 };
+
+console.log(reverseString('hello'));
 
 // Do not edit below this line
 module.exports = reverseString;
