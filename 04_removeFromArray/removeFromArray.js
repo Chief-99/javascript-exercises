0c47704e1f6ba8bffd821cb0004a3555807bff71
Similo <171865838+Chief-99@users.noreply.github.com>
2025-01-09 22:59:20 +0200

    04: Create loop to filter array for function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function(array, arguments) {
+const removeFromArray = function(array, elements) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== elements) {
+      newArray.push(array[i]);
+    }
+  }
 
+  return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
