be50f8fb960ca3cd0e97c27f27a8558c7fe1e536
Similo <171865838+Chief-99@users.noreply.github.com>
2025-01-10 14:11:53 +0200

    07: Write F to C formula

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
 const convertToCelsius = function(degreeValue) {
+  return (degreeValue - 32) * 5/9;
 };
 
 const convertToFahrenheit = function(degreeValue) {
+   
 };
 
 // Do not edit below this line
