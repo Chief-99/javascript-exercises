897bb83af3975b9598fc1d0ce0e32b7fb3813fbb
Similo <171865838+Chief-99@users.noreply.github.com>
2025-06-09 22:57:53 +0200

    07 - Complete exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = '';
+    let splitString = string.split('');
+    result = splitString.reverse().join('');
 
+    return result;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
