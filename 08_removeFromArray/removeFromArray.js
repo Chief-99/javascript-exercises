8703bc459242a5c0ea8051fce233465e121a0810
Similo <171865838+Chief-99@users.noreply.github.com>
2025-06-10 19:05:20 +0200

    08 - Complete exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    for (let value of values) {
+        while (array.includes(value)) {
+            array.splice(array.indexOf(value), 1);
+        }
+    }
+
+    return array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
