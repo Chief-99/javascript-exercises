fc374ab2c7cb57e3b447c8bcbc3d847dfbefca73
Similo <171865838+Chief-99@users.noreply.github.com>
2025-06-10 21:50:16 +0200

    09 - Complete exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    } else {
+        if (a > b) {
+            while (a >= b) {
+                sum += a;
+                a--;
+            }    
+        } else {
+            while (a <= b) {
+                sum += a;
+                a++;
+            }
+        }
+
+        return sum;
+    }
 
 };
 
