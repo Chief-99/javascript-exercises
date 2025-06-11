e5a52062885b653adc74cca994411afd784ba4f1
Similo <171865838+Chief-99@users.noreply.github.com>
2025-06-11 22:50:09 +0200

    13 - Complete exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringCopy = string.replace(/[\.,?! ]/g, '').toLowerCase();;
 
+
+    const splitString = string.split('');
+    splitString.reverse();
+    let reversedString = splitString.join('').replace(/[\.,?! ]/g, '').toLowerCase();
+
+    return stringCopy === reversedString ? true : false;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+palindromes('Racecars!');
