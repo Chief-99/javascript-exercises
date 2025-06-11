1f9fb6e3ca5e47c0e9c09d94ecb436cad9c82c99
Similo <171865838+Chief-99@users.noreply.github.com>
2025-06-11 23:35:40 +0200

    14 - Complete exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let num = Number(input);
+    let sum = 0;
+    const arrayFib = [1, 1];
+    let previous = 0;
+    let current = 0;
 
+    while ((num - 2) > 0) {
+        previous = arrayFib[arrayFib.length - 2];
+        current = arrayFib[arrayFib.length - 1];
+        sum = previous + current;
+        arrayFib.push(sum);
+        num--;
+    }
+
+    if (num === 0) {
+        return 0;
+    } else if (num < 0) {
+        return 'OOPS';
+    }
+
+    return arrayFib.pop();
 };
 
 // Do not edit below this line
