9224826ac48a76675685b85d36541a5636f3b879
Similo <171865838+Chief-99@users.noreply.github.com>
2025-07-02 16:02:21 +0200

    16 - Complete exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldestIndex = 0;
+    let currentAge = 0;
+    let oldestAge = 0;
+    let today = new Date();
 
+    array.reduce(function(oldest, current, index) {
+        if (oldest.yearOfDeath === undefined) {
+            oldest.yearOfDeath = today.getFullYear();
+        };
+
+        oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        currentAge = current.yearOfDeath - current.yearOfBirth;
+
+        if (currentAge > oldestAge) {
+            oldest = current;
+            oldestIndex = index;
+        };
+
+        return oldest;
+    }, array[0]);
+
+    return array[oldestIndex]
 };
 
 // Do not edit below this line
